e189c33c994fad77308e26fd78a33c6d14aee5a2
Alejandro Wurts <32560488+Alwurts@users.noreply.github.com>
2022-10-05 19:16:31 -0500

    Import icons

---
+++ src/components/icons/CheckIcon.tsx
@@ -0,0 +1,23 @@
+import { IconType } from "../../types";
+import { FC } from "react";
+
+const CheckIcon: FC<IconType> = ({ className }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.5 12.75l6 6 9-13.5"
+      />
+    </svg>
+  );
+};
+
+export default CheckIcon;
