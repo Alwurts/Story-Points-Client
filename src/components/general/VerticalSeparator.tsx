83b7319d2000cdc6b3ba4fdb9dc6ef2cf02b5eb2
Alejandro Wurts <32560488+Alwurts@users.noreply.github.com>
2022-10-12 21:54:27 -0500

    Add icons as components

--- src/components/general/VerticalSeparator.tsx
+++ src/components/general/VerticalSeparator.tsx
@@ -6,8 +6,11 @@ interface PropTypes {
 
 const VerticalSeparator: React.FC<PropTypes> = ({ className }) => {
   return (
-    <div className={classNames(className, "h-full py-4")} aria-hidden="true">
-      <div className="border-l-4 border-slate-900" />
+    <div
+      className={classNames(className, "h-full w-10 py-4")}
+      aria-hidden="true"
+    >
+      <div className="h-full w-10 border-l-4 border-slate-900" />
     </div>
   );
 };
