24153825e83dc707638bdfa014b2b0842cda7a4f
Alejandro Wurts <32560488+Alwurts@users.noreply.github.com>
2022-11-15 22:36:30 -0600

    Added client favicons

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,13 +12,37 @@ const Layout = ({ children, title = "This is the default title" }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
+      <meta
+        name="description"
+        content="An app that helps your team estimate agile points"
+      />
       <meta charSet="utf-8" />
       <meta
         name="viewport"
         content="initial-scale=1.0, width=device-width , maximum-scale=1.0, user-scalable=no"
       />
-      <link rel="shortcut icon" href="/favicon.ico" />
       <meta name="HandheldFriendly" content="true" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
     <ErrorBanner />
     {children}
